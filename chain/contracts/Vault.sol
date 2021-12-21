16792fed73329b5b7633ed4fe5f9e31312b1b4b7
Zsofie <zsofia@linumlabs.com>
2021-12-21 15:24:24 +0200

    added more logic

--- chain/contracts/Vault.sol
+++ chain/contracts/Vault.sol
@@ -1,15 +1,14 @@
 pragma solidity 0.8.7;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract ZVault {
+contract ZVault is IERC20 {
     address strategyContract;
 
     IERC20 public DAI;
 
     //mappings
-    mapping(address => uint256) public DAIBalance;
-    mapping(address => uint256) public yTokenBalance;
-    mapping(address => bool) public isRegisteredStrategy;
+    mapping(address => uint256) public DAIBalanceInVault;
+    mapping(address => uint256) public yTokenBalanceInVault;
     mapping(address => bool) public isRegisteredStrategy;
     mapping(address => bool) public isShutDown;
 
@@ -20,6 +19,8 @@ contract ZVault {
     event StrategyRegistered(address sContract);
     event StrategyRemoved(address sContract);
     event ShutDown(bool active);
+    event DaiMovedFromAaveToVault(uint256 amount);
+    event DaiMovedFromTreasuryToVault(uint256 amount);
 
     // modifiers
 
@@ -30,16 +31,22 @@ contract ZVault {
 
     constructor() {}
 
+    // * VIEW FUNCTIONS * //
+
     function isStrategy(address sContract) public view returns (bool) {
         return isRegisteredStrategy[sContract];
     }
 
-    function checkDaiBalance(address user) public view returns (uint256) {
-        return DAIBalance[user];
+    function getDaiBalanceInVault(address user) public view returns (uint256) {
+        return DAIBalanceInVault[user];
     }
 
-    function checkyTokenBalance(address user) public view returns (uint256) {
-        return yTokenBalance[user];
+    function getyTokenBalanceInVault(address user)
+        public
+        view
+        returns (uint256)
+    {
+        return yTokenBalanceInVault[user];
     }
 
     function setNewStrategy(address newContract)
@@ -52,13 +59,47 @@ contract ZVault {
         emit StrategyRegistered(newContract);
     }
 
-    function deposit(uint256 amount) external notShutdown {
+    function checkBalanceInAave(address user) public notShutdown {
+        // call balance of function from strategy contract
+    }
+
+    function checkBalanceInCompound(address user) public notShutdown {
+        // call balance of function from strategy contract
+    }
+
+    // * VAULT LOGIC * //
+
+    function deposit(uint256 amount) public notShutdown {
         require(DAI.transferFrom(address(this), _amount), "DEPOSIT FAILED");
         emit Deposit(amount);
     }
 
-    function withdraw(address to, uint256 amount) external notShutdown {
+    function withdraw(address to, uint256 amount) public notShutdown {
         require(to != address(0), "WITHDRAW TO ZERO ADDRESS");
         emit Withdraw(to, amount);
+    }
+
+    function moveDaiToAave(uint256 amount) public notShutdown {
+        uint256 _daiInVault = getDaiBalanceInVault();
+        require(_daiInVault >= _amount, "INSUFFICIENT FUNDS TO MOVE");
+        _moveDaiToAave(amount);
+    }
+
+    function moveDaiFromAave(uint256 amount) public notShutdown {}
+
+    function moveDaiToComp(uint256 amount) public notShutdown {
+        uint256 _daiInVault = getDaiBalanceInVault();
+        require(_daiInVault >= _amount, "INSUFFICIENT FUNDS TO MOVE");
+        _moveDaiToComp(amount);
+    }
+
+    function moveDaiFromComp(uint256 amount) public notShutdown {}
+
+    function _moveDaiToAave(uint256 _amount) internal {
+        require(_amount > 0, "NO ZERO DEPOSITS");
+    }
+
+    function _moveDaiToComp(uint256 _amount) internal {
+        require(_amount > 0, "NO ZERO DEPOSITS");
     }
 }
